ea6179f667549c6c76c73786f469733a85be7eaf
NightHavock <victorohason@gmail.com>
2026-03-18 11:03:41 +0000

    I added basic functions to a. Give the file a name b. Check when somenone visits the page c. return the index.html files d. Only run when being directly aske to run and not running being import somewhere else

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 import csv
+
+# This creates my flask application. (__name__) is a special python variable that means "This file"
+app = Flask(__name__)
+
+# a "route" is a path in my app
+@app.route('/') #when someone visits the home page(/)
+def index():#this function is called#
+    return render_template('index.html') # this then shows the index.html files from the templates folder
+
+
+
+
+
+
+if __name__ == '__main__': # This checks if this file is being run directly and not just being "imported"
+    app.run(debug=True) # this automatically reloads when i change code, really useful
